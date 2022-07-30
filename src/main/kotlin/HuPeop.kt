eaa17d38323108c4299cc18329a0626c90635eda
Захаров Роман AN-34 <ambeadexter@yandex.ru>
2022-07-30 16:17:20 +0700

    Adding exception handling of input

--- src/main/kotlin/HuPeop.kt
+++ src/main/kotlin/HuPeop.kt
@@ -1,11 +1,22 @@
 package ru.netology
 
+import java.lang.Exception
 import java.util.Scanner
 
 fun main() {
     val sc = Scanner(System.`in`)
+    var valid = true
+    var likes: String
     print("Введите количество лайков:\n>> ")
-    val likes = sc.nextLine()
+    do {
+        likes = sc.nextLine()
+        try {
+            if (likes.toInt() < 0) throw Exception()
+            valid = false
+        } catch (e: Exception) {
+            print("Введите целое число, и не меньше 0\n>> ")
+        }
+    } while (valid)
     println("Понравилось $likes ${typeDefine(likes)}")
 }
 
